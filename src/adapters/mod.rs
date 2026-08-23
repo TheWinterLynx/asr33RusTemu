bfddc25340997fa5b75bbfddaf110f9a4f68fecd
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-08-24 01:52:44 +0200

    Use CRLF-coalescing rodio backend

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -1,6 +1,6 @@
 //! Integrations with operating-system and external I/O facilities.
 
-#[path = "audio_rodio.rs"]
+#[path = "audio_rodio_crlf.rs"]
 pub mod audio;
 pub mod paper_tape;
 pub mod transport;
